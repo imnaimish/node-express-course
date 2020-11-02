d3462f209f1ebd9f764fb2e64fa320bf092cfccb
Naimish <naimish.singh@cleartrip.com>
2020-11-02 13:38:46 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 
 const mockUserData=[
 	{name:'Mark'},
 	{name:'Jill'}
 ]
+
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+	const userName = req.body.userName;
+	const password = req.body.password;
+
+	const mockUserName = 'naimishSingh'
+	const mockPassword = 'naimishSingh'
+
+	if (userName === mockUserName && password === mockPassword) {
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+	   })
+	}
+})
 
 app.get('/users/:uid', (req, res) => {
     console.log(req.params.id)
